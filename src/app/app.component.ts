fb44a773b770ebeaf270df5be86e1183be0c7087
AlbertoMasiero <alberto.masiero@scsconcept.com>
2026-01-30 16:34:44 +0100

    IonRouterOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonicModule, RouterModule],
+  imports: [IonicModule, IonRouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
