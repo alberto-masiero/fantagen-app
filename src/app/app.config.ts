790a7e5ea5aeb740c7fa1eec3436834acc2110b6
AlbertoMasiero <alberto.masiero@scsconcept.com>
2026-01-30 15:54:14 +0100

    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideRouter, RouteReuseStrategy, withHashLocation } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -8,6 +8,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideIonicAngular(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideRouter(routes, withHashLocation()),
     provideHttpClient()
   ],
