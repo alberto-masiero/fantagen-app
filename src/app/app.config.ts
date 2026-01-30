073998f9e366d75c2be09b6065e7735f4759b30d
AlbertoMasiero <alberto.masiero@scsconcept.com>
2026-01-30 15:04:20 +0100

    config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideHttpClient(),
+    provideIonicAngular(),
+    provideRouter(routes, withHashLocation()),
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-import { provideIonicAngular } from '@ionic/angular/standalone';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [
-    provideRouter(routes, withHashLocation()),
-    provideIonicAngular(),
-    ...(appConfig.providers ?? []),
-  ],
-}).catch(console.error);
+bootstrapApplication(AppComponent, appConfig).catch(console.error);
