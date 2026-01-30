b5b84a2cc7b5f5e27c43a54001cd2118143dbd04
AlbertoMasiero <alberto.masiero@scsconcept.com>
2026-01-30 14:10:42 +0100

    hash

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
+    provideRouter(routes, withHashLocation()),
     provideIonicAngular(),
     ...(appConfig.providers ?? []),
   ],
